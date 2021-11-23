d5075c400805ec7476fa745ea4a82caa31a404f0
Rafael Maia <raafamaia@gmail.com>
2021-11-23 14:32:42 -0300

    chore(commands): commands models/dtos/profile set up

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -23,18 +23,17 @@ namespace CommandsService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
